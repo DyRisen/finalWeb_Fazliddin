0741165fa7ec3a81e0467f55472bb2e36ccf609e
DyRisen <146850655+DyRisen@users.noreply.github.com>
2023-10-10 17:03:25 +0300

    Конец 1 урока

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        MyBlue1: '#1776E8',
+        MyBlue2: '#18B5F2',
+        MyBlue3: '#21D8DB',
+        MyBlue4: '#17EB76',
+        MyBlue5: '#16F4B8'
+      }
+    },
   },
   plugins: [],
 }
