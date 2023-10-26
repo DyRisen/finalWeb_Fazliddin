92969ece4aea58f297fe7c958a6a9e9bba53ac7c
DyRisen <146850655+DyRisen@users.noreply.github.com>
2023-10-26 15:50:52 +0300

    obnova

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        MyBlue1: '#1776E8',
+        MyBlue2: '#18B5F2',
+        MyBlue3: '#21D8DB',
+        MyBlue4: '#17EB76',
+        MyBlue5: '#16F4B8'
+      }
+    },
+  },
+  plugins: [],
+}
